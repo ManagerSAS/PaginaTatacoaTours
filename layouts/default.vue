f6864f8cf1c181065cb0312caec155fd6a2003db
[200~ <desarrollo@agenciamanager.com>
2023-01-16 16:40:49 -0500

    subida proyecto 16/01/2023 16:40

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app dark>
+    <v-main>
+        <Nuxt />
+    </v-main>
+      <!-- <span>&copy; {{ new Date().getFullYear() }}</span> -->
+  </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
