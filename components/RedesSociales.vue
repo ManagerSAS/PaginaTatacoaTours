0bdb06473be1707d431c7169bdf3c34d2b8fb928
[200~ <desarrollo@agenciamanager.com>
2023-01-24 09:58:34 -0500

    Actualizacion 24/04/2023 09:58

--- components/RedesSociales.vue
+++ components/RedesSociales.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flotante elevation-5 pa-2 pb-1"  >
-        <a class="Whatsapp my-2 mx-2" href="https://wa.link/tah5us" target="_blank"/>
-        <a class="Facebook my-2 mx-2" href="https://www.facebook.com/haciendacanaima1" target="_blank"/>
-        <a class="Instagram my-2 mx-2" href="https://www.instagram.com/haciendacanaima1" target="_blank"/>
+        <a class="Whatsapp my-2 mx-2" href="https://api.whatsapp.com/send?phone=573212759998" target="_blank"/>
+        <a class="Facebook my-2 mx-2" href="https://www.facebook.com/tatacoatours?mibextid=ZbWKw" target="_blank"/>
+        <a class="Instagram my-2 mx-2" href="https://www.instagram.com/tatacoatours/" target="_blank"/>
     </div>
 </template>
--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -14,12 +14,18 @@
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1019767.1113086318!2d-76.28974146875001!3d3.233773300000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3b93101376e6b3%3A0x60293206019ef063!2sTatacoa%20Tours!5e0!3m2!1ses!2sco!4v1674507353635!5m2!1ses!2sco" min-width="350" max-width="600" height="200" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </v-col>
             <v-col cols="12" lg="3" md="3" sm="12">
-                <v-row align="center" justify="center">
-                    <v-icon color="#67b539">mdi-facebook mdi-36px </v-icon><p class="Text-h2 mt-5">Tatacoa Tours</p>
-                </v-row>
-                <v-row align="center" justify="center">
-                    <v-icon color="#67b539">mdi-whatsapp mdi-36px </v-icon><p class="Text-h2 mt-5">Tatacoa Tours</p>
-                </v-row>
+                <v-btn text target="_blanck" href="https://www.facebook.com/tatacoatours?mibextid=ZbWKwL" align="center" justify="center">
+                    <v-icon color="#67b539">mdi-facebook mdi-24px </v-icon><p class="Text-h3 mt-5">Tatacoa Tours</p>
+                </v-btn> <br>
+                <v-btn text target="_blanck" href="https://api.whatsapp.com/send?phone=573212759998"  align="center" justify="center">
+                    <v-icon color="#67b539">mdi-whatsapp mdi-24px </v-icon><p class="Text-h3 mt-5">Tatacoa Tours</p>
+                </v-btn><br>
+                <v-btn text target="_blanck" href="https://www.facebook.com/profile.php?id=100063510616145&mibextid=ZbWKwL"  align="center" justify="center">
+                    <v-icon color="#67b539">mdi-facebook mdi-24px </v-icon><p class="Text-h3 mt-5">Hostal Laberintos Cuzco Tatacoa</p>
+                </v-btn><br>
+                <v-btn text target="_blanck" href="https://www.instagram.com/hostal_laberintos_cuzco_tataco/" align="center" justify="center">
+                    <v-icon color="#67b539">mdi-instagram mdi-24px </v-icon><p class="Text-h3 mt-5">Hostal Laberintos Cuzco Tatacoa</p>
+                </v-btn>
             </v-col>
         </v-row>
         <v-row align="center" justify="center">
