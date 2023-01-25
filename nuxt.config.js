571a39d700edd4a8639622b2f6913018552fa97d
[200~ <desarrollo@agenciamanager.com>
2023-01-25 09:20:27 -0500

    Actualizacion 25/04/2023 09:20

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - TatacoaToursPage',
-    title: 'TatacoaToursPage',
+    titleTemplate: 'Tatacoa Tours',
+    title: 'Tatacoa Tours',
     htmlAttrs: {
       lang: 'en'
     },
